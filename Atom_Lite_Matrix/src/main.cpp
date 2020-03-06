eb69b0f8cea9d26105946ff35135669255d27082
RIRI <ririfonfon10@gmail.com>
2020-03-06 16:25:28 +0100

    add

--- Atom_Lite_Matrix/src/main.cpp
+++ Atom_Lite_Matrix/src/main.cpp
@@ -1,125 +1,45 @@
 #include <Arduino.h>
-
-#include <Wire.h>
-
 #include "M5Atom.h"
+#include "image.h"
 
-///////////////////////////////////////PWM setting/////////////////////////////////////////
-int freq = 12000;
-int resolution = 16;
-int ledChannela = 4;
-int ledChannelb = 5;
-int ledChannelc = 6;
-int ledPina = 19;
-int ledPinb = 22;
-int ledPinc = 23;
-const boolean invert = false;    // set true if common anode, false if common cathode
+/* this Example only for M5Atom Matrix */
 
-float r = 250;
-float v = 250;
-float b = 250;
-
-int led_r = 255;
-int led_v = 255;
-int led_b = 255;
-
-///////////////////////////////////////Btn setting/////////////////////////////////////////
+extern const unsigned char AtomImageData[375 + 2];
+extern const unsigned char image_Komplex[1307];
+bool IMU6886Flag = false;
 uint8_t FSM = 0;
-
-void pwm_init()
-{
-  /////////////////////////////////////////////////PWM setup
-  ledcAttachPin(ledPina, ledChannela);
-  ledcAttachPin(ledPinb, ledChannelb);
-  ledcAttachPin(ledPinc, ledChannelc);
-  ledcSetup(ledChannela, freq, resolution);
-  ledcSetup(ledChannelb, freq, resolution);
-  ledcSetup(ledChannelc, freq, resolution);
-}
-
-void pwm_show()
-{
-  led_r = ((r * r) * 1.007852);
-  led_v = ((v * v) * 1.007852);
-  led_b = ((b * b) * 1.007852);
-  ledcWrite(ledChannela, led_r);
-  ledcWrite(ledChannelb, led_v);
-  ledcWrite(ledChannelc, led_b);
-} //leds_show
 
 void setup()
 {
   M5.begin(true, false, true);
-  delay(50);
-
-  pwm_init();
-  pwm_show();
+  delay(1000);
 }
 
 void loop()
 {
-  if (M5.Btn.wasPressed())
   {
-
-    switch (FSM)
+    if (M5.Btn.wasPressed())
     {
-    case 0:
-      r = 0;
-      v = 255;
-      b = 255;
-      M5.dis.drawpix(0, 0xf00000);
-      break;
-    case 1:
-      r = 255;
-      v = 0;
-      b = 255;
-      M5.dis.drawpix(0, 0x00f000);
-      break;
-    case 2:
-      r = 255;
-      v = 255;
-      b = 0;
-      M5.dis.drawpix(0, 0x0000f0);
-      break;
-    case 3:
-      r = 0;
-      v = 0;
-      b = 255;
-      M5.dis.drawpix(0, 0xf0f000);
-      break;
-    case 4:
-      r = 0;
-      v = 255;
-      b = 0;
-      M5.dis.drawpix(0, 0xf000f0);
-      break;
-    case 5:
-      r = 255;
-      v = 0;
-      b = 0;
-      M5.dis.drawpix(0, 0x00f0f0);
-      break;
-    case 6:
-      r = 0;
-      v = 150;
-      b = 0;
-      M5.dis.drawpix(0, 0xf0f0f0);
-      break;
-    case 7:
-      r = 255;
-      v = 255;
-      b = 255;
-      M5.dis.drawpix(0, 0x000000);
-      break;
-    }
 
-    FSM++;
-    if (FSM >= 8)
-    {
-      FSM = 0;
+      switch (FSM)
+      {
+      case 0:
+        M5.dis.animation((uint8_t *)AtomImageData, 100, LED_Display::kMoveLeft, 250);
+        break;
+      case 1:
+        M5.dis.animation((uint8_t *)image_Komplex, 100, LED_Display::kMoveLeft, 250);
+        break;
+      default:
+        break;
+      }
+      FSM++;
+      if (FSM >= 2)
+      {
+        FSM = 0;
+      }
     }
   }
-  pwm_show();
   delay(50);
+
   M5.update();
 }
--- Atom_Lite_Matrix/src/mpu6886_code.h
+++ Atom_Lite_Matrix/src/mpu6886_code.h
@@ -1,33 +1,125 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+
 #include "M5Atom.h"
 
-float accX = 0, accY = 0, accZ = 0;
-float gyroX = 0, gyroY = 0, gyroZ = 0;
-float temp = 0;
-bool IMU6886Flag = false;
+///////////////////////////////////////PWM setting/////////////////////////////////////////
+int freq = 12000;
+int resolution = 16;
+int ledChannela = 4;
+int ledChannelb = 5;
+int ledChannelc = 6;
+int ledPina = 19;
+int ledPinb = 22;
+int ledPinc = 23;
+const boolean invert = false;    // set true if common anode, false if common cathode
+
+float r = 250;
+float v = 250;
+float b = 250;
+
+int led_r = 255;
+int led_v = 255;
+int led_b = 255;
+
+///////////////////////////////////////Btn setting/////////////////////////////////////////
+uint8_t FSM = 0;
+
+void pwm_init()
+{
+  /////////////////////////////////////////////////PWM setup
+  ledcAttachPin(ledPina, ledChannela);
+  ledcAttachPin(ledPinb, ledChannelb);
+  ledcAttachPin(ledPinc, ledChannelc);
+  ledcSetup(ledChannela, freq, resolution);
+  ledcSetup(ledChannelb, freq, resolution);
+  ledcSetup(ledChannelc, freq, resolution);
+}
+
+void pwm_show()
+{
+  led_r = ((r * r) * 1.007852);
+  led_v = ((v * v) * 1.007852);
+  led_b = ((b * b) * 1.007852);
+  ledcWrite(ledChannela, led_r);
+  ledcWrite(ledChannelb, led_v);
+  ledcWrite(ledChannelc, led_b);
+} //leds_show
 
 void setup()
 {
-    M5.begin(true, false, true);
+  M5.begin(true, false, true);
+  delay(50);
 
-    if (M5.IMU.Init() != 0)
-        IMU6886Flag = false;
-    else
-        IMU6886Flag = true;
+  pwm_init();
+  pwm_show();
 }
 
 void loop()
 {
+  if (M5.Btn.wasPressed())
+  {
 
-    if (IMU6886Flag == true)
+    switch (FSM)
     {
-        M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
-        M5.IMU.getAccelData(&accX, &accY, &accZ);
-        M5.IMU.getTempData(&temp);
-
-        Serial.printf("%.2f,%.2f,%.2f o/s \r\n", gyroX, gyroY, gyroZ);
-        Serial.printf("%.2f,%.2f,%.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
-        Serial.printf("Temperature : %.2f C \r\n", temp);
+    case 0:
+      r = 0;
+      v = 255;
+      b = 255;
+      M5.dis.drawpix(0, 0xf00000);
+      break;
+    case 1:
+      r = 255;
+      v = 0;
+      b = 255;
+      M5.dis.drawpix(0, 0x00f000);
+      break;
+    case 2:
+      r = 255;
+      v = 255;
+      b = 0;
+      M5.dis.drawpix(0, 0x0000f0);
+      break;
+    case 3:
+      r = 0;
+      v = 0;
+      b = 255;
+      M5.dis.drawpix(0, 0xf0f000);
+      break;
+    case 4:
+      r = 0;
+      v = 255;
+      b = 0;
+      M5.dis.drawpix(0, 0xf000f0);
+      break;
+    case 5:
+      r = 255;
+      v = 0;
+      b = 0;
+      M5.dis.drawpix(0, 0x00f0f0);
+      break;
+    case 6:
+      r = 0;
+      v = 150;
+      b = 0;
+      M5.dis.drawpix(0, 0xf0f0f0);
+      break;
+    case 7:
+      r = 255;
+      v = 255;
+      b = 255;
+      M5.dis.drawpix(0, 0x000000);
+      break;
     }
-    delay(500);
-    M5.update();
+
+    FSM++;
+    if (FSM >= 8)
+    {
+      FSM = 0;
+    }
+  }
+  pwm_show();
+  delay(50);
+  M5.update();
 }
